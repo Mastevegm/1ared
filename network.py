613bb64ec51a6a5d8b12c50bee65fd798ee0fec7
Mastevegm <113386784+Mastevegm@users.noreply.github.com>
2022-10-10 23:32:45 -0500

    Update network.py

--- network.py
+++ network.py
@@ -18,10 +18,14 @@ import random
 
 # Third-party libraries
 import numpy as np
-
+#This is the new text
+class CrossEntropyCost(object):
+    @staticmethod
+    def fun(a,y):
+        return np.sum(np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
 class Network(object):
 
-    def __init__(self, sizes):
+    def __init__(self, sizes, cost=CrossEntropyCost):
         """The list ``sizes`` contains the number of neurons in the
         respective layers of the network.  For example, if the list
         was [2, 3, 1] then it would be a three-layer network, with the
@@ -34,18 +38,28 @@ class Network(object):
         ever used in computing the outputs from later layers."""
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
-
+        #code2###
+        self.default_weight_initializer()
+        self.cost=cost
     def feedforward(self, a):
-        """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
+            a = sigmoid(np.dot(w, a) + b)
         return a
+    def default_weight_initializer(self):
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x)/np.sqrt(x)
+                        for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+    
+    #tuve duda de esta parte porque según yo iba arriba
+  #  def feedforward(self, a):
+   #     """Return the output of the network if ``a`` is input."""
+    #    for b, w in zip(self.biases, self.weights):
+     #       a = sigmoid(np.dot(w, a)+b)
+      #  return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
+            lmbda=0.0, evaluation_data=None, monitor_evaluation_cost=False, monitor_training_cost=false, 
+            monitor_evaluation_accuracy=False, monitor_training_accuracy=False):
         """Train the neural network using mini-batch stochastic
         gradient descent.  The ``training_data`` is a list of tuples
         ``(x, y)`` representing the training inputs and the desired
@@ -55,26 +69,39 @@ class Network(object):
         epoch, and partial progress printed out.  This is useful for
         tracking progress, but slows things down substantially."""
 
-        training_data = list(training_data)
+        if evaluation_data: n_data= len(evaluation_data)
+  
         n = len(training_data)
-
-        if test_data:
-            test_data = list(test_data)
-            n_test = len(test_data)
-
+        evaluation_cost, evaluation_accuracy= [], []
+        training_cost, training_accuracy= [], []
+                      
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
-            else:
-                print("Epoch {} complete".format(j))
-
-    def update_mini_batch(self, mini_batch, eta):
+                self.update_mini_batch(mini_batch, eta, lmbda,
+                                      len(training_data))
+           
+                print("Epoch %s complete" %j)
+                if monitor_training_cost:
+                      cost=self.total_cost(training_data, lmbda)
+                      training_cost.append(cost)
+                      print("Cost data: {}".format(cost))
+                if monitor_training_accuracy:
+                      accuracy= drlf.accuracy(training_data, convert=True)
+                      training_accuracy.append(accuracy)
+                      print("accuracy data:{}/{}".format(accuracy, n))
+                if monitor_evaluation_cost:
+                      cost= self.total_cost(evaluation_data, lmbda, convert=True)
+                      evaluation_cost.append(accuracy)
+                      print("Accuracy evaluation data:{}/{}".format(self.accuracy(evaluation_data, n_data))
+                      
+        return evaluation_cost, evaluation_accuary,training_cost, training_accuracy
+                            
+                      
+    def update_mini_batch(self, mini_batch, eta, lmbda, n):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
@@ -138,7 +165,24 @@ class Network(object):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
         return (output_activations-y)
-
+    def accuracy(self, data, convert=False):
+                            if convert:
+                            results= [(np.argmax(self.feedforward(x)), np.argmax(y)
+                                       for (x,y in data)]
+   
+        else: 
+                            results = [(np.argmax(self.feedforward(x)),y)
+                                       for (x,y) in data]
+           return sum(int(x==y) for (x,y) in results )
+    def total_cost(self,data, lmbda. convert= false)
+              cost = 0.0 
+              for x,y in data: 
+                       a=seld.feedforward(x)
+                       if convert: y=vectorized_result(y)
+                     cost +=self.cost.fn(a,y) / len(data)
+               cost +- 0.5= lmbda / len(data)) * sum(np.linalg.norm(w) ** 2for w in self.weights)
+                                       
+                                       
 #### Miscellaneous functions
 def sigmoid(z):
     """The sigmoid function."""
@@ -147,3 +191,5 @@ def sigmoid(z):
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
+
+                                       
